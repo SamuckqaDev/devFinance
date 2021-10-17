dcc5e9eed79cd31b09d61468d5675df9ed08823c
Samuel Adriano Tomimatu <samuel.tomimatu@bandtec.com.br>
2021-10-17 13:36:23 -0300

    Modal and function open and close finished ;-;

---
+++ devFinance/assets/js/modal.js
@@ -0,0 +1,14 @@
+const modal = {
+  open() {
+    //Open modal;
+    // add class active to modal;
+    document.querySelector(".modal-overlay")
+    .classList.add("active");
+  },
+  close() {
+    //Close modal;
+    //remove class active to modal;
+    document.querySelector(".modal-overlay")
+    .classList.remove("active");
+  },
+};
