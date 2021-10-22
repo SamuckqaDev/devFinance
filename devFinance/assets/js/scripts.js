1942d3b4e4e7292abdd29472b275cccdf38e2a77
Samuel Adriano Tomimatu <samuel.tomimatu@bandtec.com.br>
2021-10-21 22:32:53 -0300

    Add functions configre methods amount

--- devFinance/assets/js/scripts.js
+++ devFinance/assets/js/scripts.js
@@ -1,4 +1,5 @@
 //Open and close modal;
+
 const Modal = {
   open() {
     //Open modal;
@@ -32,6 +33,12 @@ const transactions = [
     amount: -150000,
     createAt: "23/01/2021",
   },
+  {
+    id: 4,
+    description: "App",
+    amount: 180000,
+    createAt: "23/01/2021",
+  },
 ];
 
 const Transaction = {
@@ -39,6 +46,7 @@ const Transaction = {
     //Sum incomes;
   },
   outcomes() {
+    false;
     //Sum outcomes;
   },
   total() {
@@ -47,23 +55,50 @@ const Transaction = {
 };
 
 const DOM = {
+  transactionsContainer: document.querySelector("#data-table tbody"),
   addTransaction(transaction, index) {
+    console.log(transaction);
     //create Element through DOM;
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction()
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    DOM.transactionsContainer.appendChild(tr);
   },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "outcome";
 
-  innerHTMLTransaction() {
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    //Create elemnent and list infomation trough variables;
     const html = `  
-      <td class="description"></td>
-      <td class="income">R$ 170,00</td>
-      <td class="date">23/10/2021</td>
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">R$ ${amount}</td>
+      <td class="date">${transaction.createAt}</td>
       <td>
        <img src="./assets/img/minus.svg" alt="Remove transaction" />
       </td>
    `;
 
-   return html
+    return html;
+  },
+};
+
+const Utils = {
+  //Format type amount;
+  formatCurrency(value) {
+    //Add signal according to ammount;
+    const signal = Number(value) < 0 ? "-" : "";
+    //Replace value to empty-space;
+    value = String(value).replace(/\D/g, "");
+
+    //Convert to number and split to 'oneHundred';
+    value = Number(value) / 100;
+
+    //Convert number value to 'pt-br' with toLocaleString();
+    value = value.toLocaleString("pt-br", {
+      style: "currency", //Properties toLocaleString();
+      currency: "BRL", //Type amount;
+    });
+    return signal + value;
   },
 };
 
@@ -71,3 +106,8 @@ const DOM = {
 const year = document.getElementById("date");
 const newDate = new Date();
 year.innerHTML = newDate.getFullYear();
+
+//List transactions;
+transactions.forEach((transaction) => {
+  DOM.addTransaction(transaction);
+});
